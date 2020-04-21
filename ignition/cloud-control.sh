b70324c0abca0dedd6c90b52f6b908120fc9bfe8
Chris Mutchler <chris@virtualelephant.com>
2020-04-21 22:20:21 +0000

    Updates for SDDC Lab 2.0

--- ignition/cloud-control.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget https://s3-us-west-1.amazonaws.com/s3-kube-coreos/stateful-config.ign
-sudo coreos-install -d /dev/sda -i stateful-config.ign -C stable -V current -o vmware_raw
-sudo reboot
